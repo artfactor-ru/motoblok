5c4213cd3543d9311dff2b33ee66f5f36380b1ed
Roman Kazakov <romontino@gmail.com>
2021-12-23 18:16:07 +0300

    main page + apaptive+ fixed menu

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,26 @@
 'use strict';
 
+const $ = require("jquery");
+window.jQuery = $;
+
 let heroSlider = new Swiper(".hero__banner", {
     speed: 600,
-    pagination: {
-        el: ".hero__pagination",
-        clickable: true
-    },
+    breakpoints: {
+        // when window width is >= 0px
+        0: {
+            pagination: {
+                el: ".hero__pagination--mobile",
+                clickable: true
+            },
+        },
+        // when window width is >= 1025px
+        1025: {
+            pagination: {
+                el: ".hero__pagination",
+                clickable: true
+            },
+        }
+    }
 });
 
 if (heroSlider.init()) {
@@ -14,6 +29,23 @@ if (heroSlider.init()) {
         .forEach(name => {
         sliderNamesArray.push(name.innerHTML)
     })
-    document.querySelectorAll('.hero__pagination .swiper-pagination-bullet')
+    document.querySelectorAll('.hero__pagination--desktop .swiper-pagination-bullet')
         .forEach((bullet, i) =>   bullet.innerHTML = sliderNamesArray[i])
 }
+
+$('.menu-handler-js').on('click', () => {
+    $('.mega-menu').toggleClass('active')
+})
+
+let prevScrollpos = window.pageYOffset;
+window.addEventListener('scroll', function() {
+    if (pageYOffset > 120) {
+        let currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.querySelector(".header").style.top = "0";
+        } else {
+            document.querySelector(".header").style.top = "-127px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+});
